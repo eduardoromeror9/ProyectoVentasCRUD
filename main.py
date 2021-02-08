9e9e99af63e428ad2942a9864237de0cebc6ca3b
Eduardor9 <eduardoromeror9@outlook.com>
2021-02-08 04:22:22 -0300

    Comenzando proyecto de ventas

---
+++ main.py
@@ -0,0 +1,21 @@
+clients = 'Pablo, Ricardo, '
+
+def create_client(client_name):
+    global clients
+    clients += client_name
+    _add_comma()
+    
+def list_clients():
+    global clients
+    print(clients)
+    
+
+def _add_comma():
+    global clients
+    clients += ','
+
+
+if __name__ == '__main__':
+    list_clients()
+    create_client('Eduardo')
+    list_clients()
