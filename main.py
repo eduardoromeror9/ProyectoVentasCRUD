825ea2209d184b12e96a74e7ce9d4fa1d4dc1fd5
Eduardor9 <eduardoromeror9@outlook.com>
2021-02-16 04:50:46 -0300

    Ciclo while agregado

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 clients = 'Pablo, Ricardo, Hector, Miguel Eduardo, Caro, '
 
 def create_client(client_name):
@@ -42,6 +44,22 @@ def _add_comma():
     global clients
     clients += ','
     
+    
+def _get_client_name():
+    client_name = None
+    
+    while not client_name:
+        client_name = input('What is the client name? ')
+        
+        if client_name == 'exit' or client_name == 'Exit':
+            client_name = None
+            break
+        
+    if not client_name:
+        sys.exit()
+        
+    return client_name
+
 def _print_welcome():
     print('Welcome to Eduardo VENTAS!')
     print('*' * 65)
@@ -51,9 +69,7 @@ def _print_welcome():
     print('[U]pdate client')
     print('[D]elete client')
     print('[S]earch client')
-    
-def _get_client_name():
-    return input('What is the client name? ')    
+     
 
 if __name__ == '__main__':
     _print_welcome()
